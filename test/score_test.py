3345d1ed86cba1422e014f7daa64dd96ae009f87
Steven Solomon <solomon.steven.m@gmail.com>
2025-07-27 20:32:51 -0400

    Rename tests

--- test/score_test.py
+++ test/score_test.py
@@ -87,66 +87,63 @@ def test_no_win(cards: list[str]):
     assert GameResult.NoPayout == score(dealer, player)
 
 
-def test_pair_across_dealer_and_player():
+def test_pair_when_dealer_and_player_have_one_card_in_pair():
     player = ["2H", "3S", "JD"]
     dealer = ["4C", "JH"]
 
     assert GameResult.Pair == score(dealer, player)
 
 
-def test_pair_in_player_hand():
+def test_pair_when_all_cards_are_in_player_hand():
     player = ["2H", "JH", "JD"]
-    dealer = [
-        "4C",
-        "3S",
-    ]
+    dealer = ["4C", "3S"]
 
     assert GameResult.Pair == score(dealer, player)
 
 
-def test_pair_in_dealer_hand():
+def test_pair_when_all_cards_are_in_dealer_hand():
     player = ["2H", "3S", "4C"]
     dealer = ["JD", "JH"]
 
     assert GameResult.Pair == score(dealer, player)
 
 
-def test_two_pair_across_dealer_and_player_one_each_rank():
+def test_two_pair_when_dealer_and_player_have_one_of_each_rank():
     player = ["JH", "TD", "4C"]
     dealer = ["JD", "TH"]
 
     assert GameResult.TwoPair == score(dealer, player)
 
 
-def test_two_pair_across_dealer_and_player_each_has_pair():
+def test_two_pair_when_dealer_and_player_each_have_a_pair():
     player = ["TH", "TD", "4C"]
     dealer = ["JD", "JH"]
 
     assert GameResult.TwoPair == score(dealer, player)
 
 
-def test_two_pair_across_dealer_and_player_has_pair():
+def test_two_pair_when_player_has_a_pair_and_dealer_has_remaining_card():
     player = ["TH", "TD", "JH"]
     dealer = ["JD", "4C"]
 
     assert GameResult.TwoPair == score(dealer, player)
 
 
-def test_three_of_a_kind_player_hand_only():
+def test_three_of_a_kind_when_player_has_all_three_cards():
     player = ["JD", "JC", "JH"]
     dealer = ["2D", "4C"]
 
     assert GameResult.ThreeOfAKind == score(dealer, player)
 
 
-def test_three_of_a_kind_dealer_and_player():
+def test_three_of_a_kind_when_player_and_dealer_have_pair():
     player = ["JD", "JC", "4H"]
     dealer = ["JS", "4C"]
 
     assert GameResult.ThreeOfAKind == score(dealer, player)
 
 
-def test_four_of_a_kind_player_has_three_of_a_kind():
+def test_four_of_a_kind_when_player_has_three_of_a_kind():
     player = ["JD", "JC", "JS"]
 
     dealer = ["JH", "4C"]
@@ -154,7 +151,7 @@ def test_four_of_a_kind_player_has_three_of_a_kind():
     assert GameResult.FourOfAKind == score(dealer, player)
 
 
-def test_four_of_a_kind_player_and_dealer_have_pairs():
+def test_four_of_a_kind_when_player_and_dealer_have_pairs():
     player = ["JD", "JC", "4C"]
 
     dealer = ["JH", "JS"]
